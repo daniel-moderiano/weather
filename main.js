810037763ae3972321ed8a09730eff95d3cc96b7
Daniel <daniel.moderiano@outlook.com>
2021-08-10 21:21:08 +0930

    Cleaned up comments

--- main.js
+++ main.js
@@ -1,21 +1,72 @@
-const weatherDiv = document.querySelector('.weather');
+const weatherTemperature = document.querySelector('.weather__temperature');
+const weatherDescription = document.querySelector('.weather__description');
+const weatherHumidity = document.querySelector('.weather__humidity');
+const weatherFeelsLike = document.querySelector('.weather__feels-like');
+const searchBar = document.querySelector('.search__bar');
+const searchBtn = document.querySelector('.search__btn');
 
-const cityName = 'Sydney';
-
-function formatTemperatureInCelsius(temperature) {
-  weatherDiv.textContent = temp + '°C';
+function formatWeatherData(data) {
+  return {
+    temperature: data.main.temp,
+    description: data.weather[0].description,
+    humidity: data.main.humidity,
+    feelsLike: data.main['feels_like'],
+  }
 }
 
-const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&id=524901&appid=80c5a14cc53f0fe21d2bb89222f9a766`;
+searchBtn.addEventListener('click', () => {
+  const searchQuery = searchBar.value;
+  if (searchQuery === "") {
+    throw new Error("City name must be entered")
+  } else {
+    promiseFetch(searchQuery);
+  }
+   
+})
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&id=524901&appid=80c5a14cc53f0fe21d2bb89222f9a766`, { mode: 'cors' })
-  .then((response) => response.json())
 
-  // Do something with the JSON API data
+// ### PROMISE VERSION ###
+
+function promiseFetch(cityName) {
+
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&id=524901&appid=80c5a14cc53f0fe21d2bb89222f9a766`, { mode: 'cors' })
   .then((response) => {
-    const temperature = response.main.temp;
-    weatherDiv.textContent = temperature.toFixed(1) + '°C';
+    if (!response.ok) {
+      throw Error(response.statusText);
+    } 
+    return response.json();
   })
+  .then((response) => {
+    const weatherData = formatWeatherData(response);
+    weatherTemperature.textContent = `${weatherData.temperature.toFixed(1)}°C`;
+    weatherDescription.textContent = weatherData.description;
+    weatherHumidity.textContent = `${weatherData.humidity}% humidity`;
+    weatherFeelsLike.textContent = `Feels like ${weatherData.feelsLike.toFixed(1)}°C`;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
 
-  // Do something with the error data
-  .catch((error) => console.log(error));
+// promiseFetch();
+
+
+
+// ### ASYNC/AWAIT VERSION
+
+async function asyncFetch(cityName) {
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&id=524901&appid=80c5a14cc53f0fe21d2bb89222f9a766`, { mode: 'cors' });
+    const APIData = await response.json();
+    const weatherData = formatWeatherData(APIData);
+
+    weatherTemperature.textContent = `${weatherData.temperature.toFixed(1)}°C`;
+    weatherDescription.textContent = weatherData.description;
+    weatherHumidity.textContent = `${weatherData.humidity}% humidity`;
+    weatherFeelsLike.textContent = `Feels like ${weatherData.feelsLike.toFixed(1)}°C`;
+  } catch (error) {
+    console.log(error);
+  }  
+}
+
+// asyncFetch();
